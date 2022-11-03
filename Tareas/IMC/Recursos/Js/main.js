3b6542ebdcc3a09abfb26c1b6d4f1c9d49ea5959
CesarOtero9 <Cesar.OVega19@gmail.com>
2022-11-03 02:13:43 -0600

    Teminando la ventana modal

---
+++ Tareas/IMC/Recursos/Js/main.js
@@ -0,0 +1,28 @@
+let cerrar = document.querySelectorAll(".close")[0];
+let abrir = document.querySelector(".calcular");
+let modal = document.querySelectorAll(".modal")[0];
+let modalC = document.querySelectorAll(".modalCont")[0];
+
+console.log (cerrar);
+console.log (abrir);
+console.log (modal);
+console.log (modalC);
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modalClose");
+});
+
+cerrar.addEventListener("click", function(){
+    modal.classList.toggle("modalClose");
+
+    setTimeout(function(){
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+    },900)
+});
+
+
+
