f06334f979725a0f2d66d6d648bbd7258797bdc8
CesarOtero9 <Cesar.OVega19@gmail.com>
2022-11-09 00:34:50 -0600

    Calculadora2

---
+++ Tareas/IMC/Recursos/Js/warnings.js
@@ -0,0 +1,9 @@
+const nom = document.getElementById("nom")
+const pes = document.getElementById("peso")
+const alt = document.getElementById("estatura")
+const edad = document.getElementById("edad")
+const parrafo = document.getElementById("warnings")
+const form = document.getElementById("form")
+
+
+
